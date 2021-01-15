9e1f4bb1486e9e6c5553c8264f55b238c99a771c
boomwerk <aouachria.yannis@hotmail.fr>
2021-01-15 16:58:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  
+  constructor(props) {
+    
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: [],
+      search : ""
+    };
+    
+  }
+ 
+  componentDidMount(){
+    
+    
+
+    fetch("http://localhost:3000/mock.json").then(function(response){
+      
+     return response.json()
+
+    }).then((data)=>{
+      
+      this.setState({
+        data : data
+      })
+
+    })
+
+    
+
+    
+  }
+
+  onChange = (e) => {
+
+    this.setState({
+      search : e.target.value
+    })
+    
+  }
+
+  renderuser(user){
+    const {search} = this.state
+
+    if(search !== "" && user.first_name.toLowerCase().indexOf( search.toLowerCase() ) === -1){
+      return null 
+    }
+
+    return <li>
+            <img src={user.photo} /> {user.first_name} 
+          </li>
+  }
+
+
+  
+
+  render() {
+  
+    const {data} = this.state;
+
+    
+    
+     
+
+
+    
+     
+    return (
+      
+      <div class="container">
+        <form>
+          <input type="search" icon="search" onChange={this.onChange}/>
+        </form>
+        <ul>
+          {
+            data.map(user => {
+              return this.renderuser(user)
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+  
 }
+
+
+
+
+
 
 export default App;
